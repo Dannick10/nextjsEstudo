dec336426d9321e1d913c45c64fd55f5e733d1f7
unknown <danmaggot2@gmail.com>
2024-06-17 19:43:21 -0300

    primeiro componente em next

---
+++ exercicio/src/components/PrimeiroComponent.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const PrimeiroComponent = () => {
+  return (
+    <div className='text-center'>
+        <h2 className='text-xl font-bold text-slate-600'>Primeiro componente</h2>
+        <p className='text-xs'>Criando e exportando o primeiro componente em next</p>
+    </div>
+  )
+}
+
+export default PrimeiroComponent
