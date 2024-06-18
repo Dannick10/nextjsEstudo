5ab37fac7db8dc89d49fe2aab4caeb4fbb7f4c48
unknown <danmaggot2@gmail.com>
2024-06-18 15:28:54 -0300

    import style

--- exercicio/src/components/fragment1.tsx
+++ exercicio/src/components/fragment1.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-
+import styles from './fragment.module.css'
 
 const Fragment1 = (Props:any) => {
 
   return (
     <>
-      <h1>{Props.princial}</h1>
+      <h1 className={styles.h1}>{Props.princial}</h1>
       <h2>{Props.secundario}</h2>
     </>
   );
