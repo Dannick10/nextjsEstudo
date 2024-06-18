6bcedf1b4c0eec3c440ae9afa611af9a7d346611
unknown <danmaggot2@gmail.com>
2024-06-18 16:20:48 -0300

    my solutin

---
+++ tabuleiro/src/Components/Tabuleiro.tsx
@@ -0,0 +1,27 @@
+import { table } from "console";
+import React from "react";
+
+const Tabuleiro = () => {
+  const generateTable = (num: number): JSX.Element[] => {
+    const Table: JSX.Element[] = [];
+    for (let i = 0; i <= num; i++) {
+      Table.push(
+        <>
+          <div
+            className={`w-[5em] h-[5em] ${
+              i % 2 ? "bg-white text-black" : "bg-black text-white"
+            } flex items-center justify-center`}
+          >
+            {i}
+          </div>
+        </>
+      );
+    }
+
+    return Table;
+  };
+
+  return <div className="flex flex-wrap w-[50%]">{generateTable(80)}</div>;
+};
+
+export default Tabuleiro;
---
+++ tabuleiro/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Tabuleiro from '../Components/Tabuleiro'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Tabuleiro/>
+
+
+    </main>
+  );
+}
