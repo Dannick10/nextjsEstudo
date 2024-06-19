94763eec10ee7a2d2a057ddc4a5a47c1f50d0ad4
unknown <danmaggot2@gmail.com>
2024-06-19 16:32:16 -0300

    onlcick e onchange

---
+++ exercicio/src/pages/comunicacaoindireta/Button.tsx
@@ -0,0 +1,24 @@
+import React, {useState} from 'react'
+
+type Props = {}
+
+const Button = (props: Props) => {
+
+    const [click,Setclick] = useState(false)
+
+    const action = (e) => {
+       Setclick(click ? false : true)
+       console.log(e)
+    }
+
+  return (
+    <div>
+        <input type="text" onChange={(e) => console.log(e.target.innerHTML)} />
+        <button onClick={action}>Click #01</button>
+        <button onClick={() => Setclick(click ?false :true)}>Click #02</button>
+        {click ? 'ta clicado' : 'desclicado'}
+    </div>
+  )
+}
+
+export default Button
