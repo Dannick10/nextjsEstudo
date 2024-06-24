d9bb1d40391ac927c93ed69275d8c3675e9b9a1f
unknown <danmaggot2@gmail.com>
2024-06-24 15:20:49 -0300

    rotas

--- next-basico/src/app/page.tsx
+++ next-basico/src/app/page.tsx
@@ -1,7 +1,18 @@
+'use client'
+
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import path from "path";
 
 export default function Home() {
+
+  const router = useRouter()
+
+  const navigation = (url:string) => {
+    router.push(url)
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <ul>
@@ -11,6 +22,10 @@ export default function Home() {
         </li>
           </Link>
       </ul>
+      <div className="flex flex-col">
+        <button onClick={() => router.push('/rotas/buscar')}>Buscar</button>
+        <button onClick={() => navigation('daniel/123')}>Daniel</button>
+      </div>
     </main>
   );
 }
