2b5341e1f10d13333aa0401199a79e22fc9741ec
unknown <danmaggot2@gmail.com>
2024-06-27 16:47:50 -0300

    porta

---
+++ multiroom/src/app/components/Porta.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Porta = (props: Props) => {
+  return (
+    <div className='flex flex-col w-[200px] h-[310px] bg-orange-600 items-center p-10 text-white'>
+        <div className='text-5xl'>3</div>
+    </div>
+  )
+}
+
+export default Porta
--- multiroom/src/app/page.tsx
+++ multiroom/src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Presente from "./components/Presente";
+import Porta from "./components/Porta";
 
 export default function Home() {
   return (
     <main>
-      <Presente />
+      <Porta />
       
     </main>
   );
