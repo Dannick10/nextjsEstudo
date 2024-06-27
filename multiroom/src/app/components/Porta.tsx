548693aca96670c1f02424efd7398288f9dd1ee5
unknown <danmaggot2@gmail.com>
2024-06-27 17:18:22 -0300

    finalizando porta

--- multiroom/src/app/components/Porta.tsx
+++ multiroom/src/app/components/Porta.tsx
@@ -4,12 +4,13 @@ type Props = {};
 
 const Porta = (props: Props) => {
   return (
-    <div className="flex w-[200px] h-[300px] relative">
+    <div className="flex justify-center w-[200px] h-[300px] relative">
       <div className="flex flex-col bg-orange-600 items-center p-10 text-white flex-grow-1 border-4 border-red-700 border-b-0">
         <div className="flex flex-col bg-orange-600 items-center p-10 text-white flex-grow-1"></div>
         <div className="text-5xl">3</div>
       </div>
-        <div className="flex flex-col bg-white items-center text-white flex-grow-1 absolute -left-[10%] bottom-0 h-[1em] w-[100%]"></div>
+        <div className="bg-white flex-grow-1 absolute h-[1em] w-[100%] bottom-0"></div>
+        <div className="flex flex-col bg- items-centerflex-grow-1 absolute h-[1.2em] w-[1.2em] bg-orange-900 right-8 rounded-full top-[50%]"></div>
     </div>
   );
 };
