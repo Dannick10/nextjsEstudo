64382a2173e17667986c185fce3b6094a54fcdfe
unknown <danmaggot2@gmail.com>
2024-07-04 15:19:03 -0300

    :feat new module about render nextjs | first | estatico

---
+++ prerendernextjs/src/pages/estatico.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react'
+
+type Props = {}
+
+const estatico = (props: Props) => {
+//const [num, Setnum] = useState<number>()
+
+   /* useEffect(() => {
+        Setnum(Math.random())
+    },[])*/
+
+  return (
+    <div>
+        <h2>Estático #02</h2>
+        {/*num*/}
+    </div>
+  )
+}
+
+export default estatico
