9f355d8f6fdbbb8b442213ba263006c7f77adf08
unknown <danmaggot2@gmail.com>
2024-07-08 16:53:30 -0300

    diferença do getstatic pro serverside

--- prerendernextjs/src/pages/api/produtos.tsx
+++ prerendernextjs/src/pages/api/produtos.tsx
@@ -1,8 +1,12 @@
+function randomNumber(min = 1, max = 10000) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 export default function handle(req: Request, res: any) {
   res.status(200).json([
-    { id: 1, nome: "caneta", preco: 2.2 },
-    { id: 2, nome: "lapis", preco: 1.9 },
-    { id: 3, nome: "borracha", preco: 1.25 },
-    { id: 4, nome: "piloto", preco: 5.6 },
+    { id: randomNumber(), nome: "caneta", preco: 2.2 },
+    { id: randomNumber(), nome: "lapis", preco: 1.9 },
+    { id: randomNumber(), nome: "borracha", preco: 1.25 },
+    { id: randomNumber(), nome: "piloto", preco: 5.6 },
   ]);
 }
