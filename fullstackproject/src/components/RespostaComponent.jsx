c3e218461af2b250ff604aef83a5f4a2d6f9742c
unknown <danmaggot2@gmail.com>
2024-07-11 18:28:32 -0300

    css resposta

--- fullstackproject/src/components/RespostaComponent.jsx
+++ fullstackproject/src/components/RespostaComponent.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const RespostaComponent = ({ resposta, letra, indice, corletra }) => {
   console.log(resposta);
   return (
-    <div className="flex p-2">
-      <div className="">
-        <div className="" style={{ background: corletra }}>
-          {letra}
+    <div className="flex p-2 bg-white text-black font-bold rounded-md">
+      <div className="flex justify-between items-center min-w-52">
+        <div className="h-[40px] w-[40px] flex justify-center items-center text-center rounded-full" style={{ background: corletra }}>
+          <p>{letra}</p>
         </div>
         <div className="">{resposta.valor}</div>
       </div>
