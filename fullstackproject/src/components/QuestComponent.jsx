8cd109ec24a457775bf1b0be056a7e4c08df4d83
unknown <danmaggot2@gmail.com>
2024-07-11 15:06:24 -0300

    criando component

---
+++ fullstackproject/src/components/QuestComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const QuestComponent = (props) => {
+  console.log(props)
+
+  return (
+    <div className='flex'>
+
+    </div>
+  )
+}
+
+export default QuestComponent
--- fullstackproject/src/pages/api/bancodDeQuestoes.js
+++ fullstackproject/src/pages/api/bancodDeQuestoes.js
@@ -2,24 +2,7 @@ import questalModel from "../../../model/questao";
 import RespostaModel from "../../../model/resposta";
 
 const questoes = [
-  new questalModel(306, "javascript é uma linguagem de orientaçao a objeto?", [
-    RespostaModel.certa("Sim"),
-    RespostaModel.errada("Não"),
-  ]),
-  new questalModel(202, `como se declara uma variavel de "nome" em js?`, [
-    RespostaModel.errada("var = nome"),
-    RespostaModel.certa("var nome = "),
-    RespostaModel.errada("nome ="),
-  ]),
-  new questalModel(104, "como printa algo na tela com js?", [
-    RespostaModel.errada("echo"),
-    RespostaModel.errada("print"),
-    RespostaModel.certa("console.log"),
-  ]),
-  new questalModel(354, "qual o mais usado em js?", [
-    RespostaModel.errada(":"),
-    RespostaModel.certa(";"),
-  ]),
+  
 ];
 
 export default questoes;
