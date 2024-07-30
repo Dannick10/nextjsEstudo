94cfc3355405c9e5f20ffd187cfda806e1a71f93
unknown <danmaggot2@gmail.com>
2024-07-30 15:02:24 -0300

    component Titulo

--- admintemplate/src/components/template/Titulo.tsx
+++ admintemplate/src/components/template/Titulo.tsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React from "react";
 
 type TituloProps = {
-    titulo: string
-    subtitulo: string 
-}
+  titulo: string;
+  subtitulo: string;
+};
 const Titulo = (props: TituloProps) => {
   return (
     <div>
-        <h1 className={`
-        
-        `}>{props.titulo}</h1>
-        <h2 className={``}>{props.subtitulo}</h2>
+      <h1
+        className={`
+          font-black text-3xl text-gray-900
+        `}
+      >
+        {props.titulo}
+      </h1>
+      <h2 className={`font-light text-sm text-gray-600`}>{props.subtitulo}</h2>
     </div>
-  )
-}
+  );
+};
 
-export default Titulo
+export default Titulo;
