3917b066256472d09e7c677fe25157619e9b79b1
unknown <danmaggot2@gmail.com>
2024-07-30 16:16:51 -0300

    button logout

--- admintemplate/src/components/template/MenuItems.tsx
+++ admintemplate/src/components/template/MenuItems.tsx
@@ -2,22 +2,39 @@ import React from "react";
 import Link from "next/link";
 
 type MenuItemProps = {
-  url: string;
+  url?: string;
   texto: string;
   icone: any;
+  onClick?: (event: any) => void;
+  classname?: string
 };
 
 const MenuItems = (props: MenuItemProps) => {
+  const renderizarConteudo = () => {
+    return (
+      <p className="flex flex-col justify-center items-center h-20 w-full">
+        {props.icone}
+        <span className={`text-xs font-light`}>
+          {props.texto}
+        </span>
+      </p>
+    );
+  };
+
   return (
-    <li className={`hover:bg-green-100`}>
-      <Link href={props.url}>
-        <p className="flex flex-col justify-center items-center h-20 w-full">
-          {props.icone}
-          <span className={`text-xs font-light text-gray-600`}>
-            {props.texto}
-          </span>
-        </p>
-      </Link>
+    <li className={`hover:bg-green-100 text-gray-600 ${props.classname}`}>
+      {props.url ? (
+        <Link href={props.url}>
+          {renderizarConteudo()}
+          </Link>
+      ) : (
+        <li
+          className={`cursor-pointer`}
+          onClick={props.onClick}
+        >
+          {renderizarConteudo()}
+        </li>
+      )}
     </li>
   );
 };
--- admintemplate/src/components/template/MenuLateral.tsx
+++ admintemplate/src/components/template/MenuLateral.tsx
@@ -1,22 +1,26 @@
-import React from 'react'
-import MenuItems from './MenuItems'
-import { IconeAjustes, IconeCase, IconeSino } from '../icons'
-import Logo from './Logo'
-
+import React from "react";
+import MenuItems from "./MenuItems";
+import { IconeAjustes, IconeCase, IconeSair, IconeSino } from "../icons";
+import Logo from "./Logo";
 
 const MenuLateral = () => {
   return (
-    <div className={``}>
-      <div className={`bg-gradient-to-r from-indigo-500 to-purple-800 h-20 w-20 flex flex-col items-center justify-center`}>
-        <Logo/>
+    <div className={`flex flex-col`}>
+      <div
+        className={`bg-gradient-to-r from-indigo-500 to-purple-800 h-20 w-20 flex flex-col items-center justify-center`}
+      >
+        <Logo />
       </div>
-      <ul>
-          <MenuItems url='/' texto='Inicio' icone={IconeCase} />
-          <MenuItems url='/ajustes' texto='Ajustes' icone={IconeAjustes} />
-          <MenuItems url='/notificacoes' texto='Notificações' icone={IconeSino}/>
+      <ul className="flex-grow">
+        <MenuItems url="/" texto="Inicio" icone={IconeCase} />
+        <MenuItems url="/ajustes" texto="Ajustes" icone={IconeAjustes} />
+        <MenuItems url="/notificacoes" texto="Notificações" icone={IconeSino} />
       </ul>
-      </div>
-  )
-}
+      <ul>
+        <MenuItems  texto="Sair" icone={IconeSair} onClick={() => {console.log('clicou')}} classname="text-red-600 hover:bg-red-400 hover:text-white"/>
+      </ul>
+    </div>
+  );
+};
 
-export default MenuLateral
+export default MenuLateral;
