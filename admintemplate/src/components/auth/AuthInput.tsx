d01f8addd8a1f222d206abfb589bc91b0d611cd3
unknown <danmaggot2@gmail.com>
2024-08-09 14:42:40 -0300

    autentificação2

--- admintemplate/src/components/auth/AuthInput.tsx
+++ admintemplate/src/components/auth/AuthInput.tsx
@@ -11,13 +11,14 @@ type AuthInputProps = {
 
 const AuthInput = (props: AuthInputProps) => {
   return props.naoRenderizarQuando ? null : (
-    <div>
+    <div className="flex flex-col mt-4">
       <label htmlFor="">{props.label}</label>
       <input
         type={props.tipo}
         value={props.valor}
         onChange={(e) => props.valorMudou?.(e.target.value)}
         required={props.obrigatorio}
+        className="px-4 py-3 rounded-lg bg-blue-200 mt-2 focus:bg-white focus:border-blue-500 border outline-none"
       />
     </div>
   );
--- admintemplate/src/pages/autentificacao.tsx
+++ admintemplate/src/pages/autentificacao.tsx
@@ -1,20 +1,59 @@
 import AuthInput from "@/components/auth/AuthInput";
+import { IconeGoogle } from "@/components/icons";
 import React, { useState } from "react";
 
 type Props = {};
 
 const autentificacao = (props: Props) => {
-const [modo, Setmodo] = useState<'login' | 'cadastro'>('login')
+  const [modo, Setmodo] = useState<"login" | "cadastro">("login");
   const [email, SetEmail] = useState("");
   const [senha, SetSenha] = useState("");
 
-  return (
-    <div>
-      <h1>Autentificação</h1>
-      <AuthInput label="Email" valor={email} tipo="email" valorMudou={SetEmail} obrigatorio/>
-      <AuthInput label="Senha" valor={senha} tipo="password" valorMudou={SetSenha} obrigatorio/>
-      <AuthInput label="Senha" valor={senha} tipo="password" valorMudou={SetSenha} obrigatorio/>
+  function submeter() {
+    if (modo === "login") {
+      console.log("login");
+    } else {
+      console.log("cadastrar");
+    }
+  }
 
+  return (
+    <div className="flex justify-center items-center flex-col h-screen">
+      <div className="w-1/2">
+        <h1 className={`text-xl font-bold mb-5 `}>
+          {modo === "login"
+            ? "Entre com a Sua Conta"
+            : "Cadastre-se na plataforma"}
+        </h1>
+        <AuthInput
+          label="Email"
+          valor={email}
+          tipo="email"
+          valorMudou={SetEmail}
+          obrigatorio
+        />
+        <AuthInput
+          label="Senha"
+          valor={senha}
+          tipo="password"
+          valorMudou={SetSenha}
+          obrigatorio
+        />
+       
+        <button
+          onClick={submeter}
+          className={`w-full bg-indigo-500 hover:bg-indigo-400 text-white rounded-lg px-4 py-3 mt-6`}
+        >
+          {modo === "login" ? "Entrar" : "Cadastrar"}
+        </button>
+        <hr className="my-6 border-gray-300 w-full" />
+        <button
+          onClick={submeter}
+          className={`w-full bg-red-500 hover:bg-red-400 text-white rounded-lg px-4 py-3  flex items-center justify-center gap-2`}
+        >
+          {IconeGoogle} Entrar com Google
+        </button>
+      </div>
     </div>
   );
 };
