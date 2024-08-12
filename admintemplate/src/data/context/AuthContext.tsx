070c4ce59ab1c14092dc20689ae07bce381ee022
unknown <danmaggot2@gmail.com>
2024-08-12 15:56:18 -0300

    cookies para guardar usuario

--- admintemplate/src/data/context/AuthContext.tsx
+++ admintemplate/src/data/context/AuthContext.tsx
@@ -1,10 +1,11 @@
 import { promises } from "dns";
 import firebase from "../../firebase/config";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { auth } from "../../firebase/config";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup, onIdTokenChanged } from "firebase/auth";
 import Usuario from "@/model/Usuario";
 import Router from "next/router";
+import Cookies from "js-cookie";
 
 interface AuthContextProps {
   usuario?: Usuario;
@@ -25,16 +26,48 @@ async function usuarioNormalizado(usuarioFirebase: firebase.User) {
   };
 }
 
+function gerenciarCookie(logado: boolean) {
+  if (logado) {
+    Cookies.set('admin-template-code', String(logado), {
+      expires: 7
+    });
+  } else {
+    Cookies.remove('admin-template-code');
+  }
+}
+
 export function AuthProvider(props) {
-  const [usuario, Setusuario] = useState<Usuario>(null);
+  const [carregando, setCarregando] = useState(true);
+  const [usuario, setUsuario] = useState<Usuario>(null);
 
   async function loginGoogle() {
     const provider = new GoogleAuthProvider();
     const resp = await signInWithPopup(auth, provider);
     const user = await usuarioNormalizado(resp.user);
-    Setusuario(user);
+    configurarSession(resp.user);
+    setUsuario(user);
     Router.push("/");
   }
+
+  async function configurarSession(usuarioFirebase) {
+    if (usuarioFirebase?.email) {
+      const usuario = await usuarioNormalizado(usuarioFirebase);
+      setUsuario(usuario);
+      gerenciarCookie(true);
+      setCarregando(false);
+      return usuario.email;
+    } else {
+      setUsuario(null);
+      gerenciarCookie(false);
+      setCarregando(true);
+      return false;
+    }
+  }
+
+  useEffect(() => {
+    const cancelar = onIdTokenChanged(auth, configurarSession)
+    return () => cancelar()
+  },[])
 
   return (
     <AuthContext.Provider
