61c2f6331fa4818b68f3f686d322f6b8a6eec333
unknown <danmaggot2@gmail.com>
2024-08-14 14:31:45 -0300

    estilo tabela

--- admintemplate/src/components/template/table/TableColumns.tsx
+++ admintemplate/src/components/template/table/TableColumns.tsx
@@ -13,9 +13,9 @@ const TableColumns = (props: TableColumnsProps) => {
     const renderizarCabecalho = () => {
         return (
             <tr>
-            <th>Codigo</th>
-            <th>nome</th>
-            <th>idade</th>
+            <th className="p-4">Codigo</th>
+            <th className="p-4">nome</th>
+            <th className="p-4">idade</th>
       </tr>
         )
     }
@@ -24,10 +24,10 @@ const TableColumns = (props: TableColumnsProps) => {
     const renderizarCorpo = () => {
       return props.clientes?.map((cliente,i) => {
         return (
-            <tr key={cliente.id}> 
-                <td>{cliente.id}</td>
-                <td>{cliente.nome}</td>
-                <td>{cliente.idade}</td>
+            <tr key={cliente.id} className={`${i%2 ? 'bg-purple-300' : 'bg-purple-100'}`}> 
+                <td className="p-4">{cliente.id}</td>
+                <td className="p-4">{cliente.nome}</td>
+                <td className="p-4">{cliente.idade}</td>
             </tr>
         )
       })
@@ -36,8 +36,8 @@ const TableColumns = (props: TableColumnsProps) => {
 
 
   return (
-    <table>
-        <thead>
+    <table className="w-full text-left">
+        <thead className="bg-gradient-to-r from-purple-500 to-purple-600 p-4 rounded-lg">
       {renderizarCabecalho()}
         </thead>
         <tbody>
